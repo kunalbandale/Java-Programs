cafca1df28d3ace52ee9f88b9e1bd878318a8ffc
Kunal Bandale <bandalekunal@gmail.com>
2023-03-17 11:08:26 +0530

    Add files via upload

---
+++ UserDefinedMethods.java
@@ -0,0 +1,15 @@
+public class UserDefinedMethods {
+    public static void main(String[] args) {
+//        method
+//        returnType functionName { arguments} {}
+
+       System.out.println(average(34,45));
+
+        }
+    static int average (int firstNumber , int secondNumber) {
+        int sum = firstNumber + secondNumber;
+        return sum/2;
+
+
+    }
+}
