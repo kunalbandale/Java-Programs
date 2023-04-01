b246acd43665f92746980836a18d6cfd715ea6c8
Kunal Bandale <bandalekunal@gmail.com>
2023-04-01 10:10:21 +0530

    Add files via upload

---
+++ AssigmentOperator.java
@@ -0,0 +1,12 @@
+public class AssigmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        a+=4;
+        b-=4;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ UnaryOperators.java
@@ -0,0 +1,11 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println(a++); // 20
+        System.out.println(++a); // 22
+        System.out.println(a--); // 22
+        System.out.println(--a); // 20
+        System.out.println("-------");
+        System.out.println(a);   // 20
+    }
+}
