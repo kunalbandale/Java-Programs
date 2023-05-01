a7179b1a1d1983a2a894dbc311610cbfb7e7d31f
Kunal Bandale <bandalekunal@gmail.com>
2023-05-01 19:10:31 +0530

    Add files via upload

---
+++ TypeCastingInJava.java
@@ -0,0 +1,9 @@
+public class TypeCastingInJava {
+    public static void main(String[] args) {
+        double d = 45.34;
+        System.out.println(d);
+
+        int i = (int) d;
+        System.out.println(i);
+    }
+}
